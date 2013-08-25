a88e9c6f9d3ea76b878fbad88a537a58df1f2d42
styhihs <styhihs@gmail.com>
2013-08-26 00:25:33 +0800

    modify 11292.cpp

--- 11292.cpp
+++ 11292.cpp
@@ -4,32 +4,32 @@ using namespace std;
 
 int main()
 {
-	int head[20001], knight[20001];
-	int n, m, i, j, cost;
-	
-	while(scanf("%d %d", &n, &m) && n && m) {
-		for(i = 0; i < n; i++)
-			scanf("%d", &head[i]);
-		for(i = 0; i < m; i++)
-			scanf("%d", &knight[i]);
-			
-		sort(head, head + n);
-		sort(knight, knight + m);
-		
-		cost = 0;
-		i = 0;	// head to be chopped off
-		for(j = 0; j < m && i < n; j++) {
-			if(knight[j] >= head[i]) {
-				cost += knight[j];
-				i++;
+    int head[20001], knight[20001];
+    int n, m, i, j, cost;
+    
+    while(scanf("%d %d", &n, &m) && n && m) {
+        for(i = 0; i < n; i++)
+            scanf("%d", &head[i]);
+        for(i = 0; i < m; i++)
+            scanf("%d", &knight[i]);
+        
+        sort(head, head + n);
+        sort(knight, knight + m);
+        
+        cost = 0;
+        i = 0;	// head to be chopped off
+        for(j = 0; j < m && i < n; j++) {
+            if(knight[j] >= head[i]) {
+                cost += knight[j];
+                i++;
             }
-		}
-		
-		if(i == n)
+        }
+        
+        if(i == n)
             printf("%d\n", cost);
         else
             printf("Loowater is doomed!\n");
-	}
-	
-	return 0;
+    }
+    
+    return 0;
 }
